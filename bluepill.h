b98dc2bde3e2a9cec527ce0231e6a56b63618db6
Adam Frączek <adam.fraczek.krk@gmail.com>
2018-06-21 20:51:02 +0200

    initial

---
+++ bluepill.h
@@ -0,0 +1,37 @@
+#ifndef BLUEPILL_H_   /* DEFINE LIST FOR BLUE PILL USAGE IN ATMEL STUDIO */
+#define BLUEPILL_H_
+
+#define PB11	PB11
+#define PB10	PB10
+#define PB1		PB1
+#define PB0		PB0
+#define PA7		PA7
+#define PA6		PA6
+#define PA5		PA5
+#define PA4		PA4
+#define PA3		PA3
+#define PA2		PA2
+#define PA1		PA1
+#define PA0		PA0
+#define PC15	PC15
+#define PC14	PC14
+#define PC13	PC13
+#define PB12	PB12
+#define PB13	PB13
+#define PB14	PB14
+#define PB15	PB15
+#define PA8		PA8
+#define PA9		PA9
+#define PA10	PA10
+#define PA11	PA11
+#define PA12	PA12
+#define PA15	PA15
+#define PB3		PB3
+#define PB4		PB4
+#define PB5		PB5
+#define PB6		PB6
+#define PB7		PB7
+#define PB8		PB8
+#define PB9		PB9
+
+#endif // BLUEPILL_H_
